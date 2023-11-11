4f99a809903bd3e414834112624ae8df04f5567b
Nilesh Dubey <nileshdubey@Nileshs-MacBook-Pro.local>
2023-11-11 05:38:05 +0530

    reactjs 3rd repo

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,13 @@
 // import logo from './logo.svg';
 import React from 'react'
 import './App.css';
-import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Navbar from './components/Navbar';
 import Home from './components/Home';
 import Footer from './components/Footer';
 import SignUp from './components/SignUp';
 import "./styles/SignUp.css";
-import Menu  from './components/Menu';
+import Menu from './components/Menu';
 import About from './components/About';
 import Contact from './components/Contact.jsx';
 
@@ -15,15 +15,15 @@ function App() {
   return (
     <div className="App">
       <Router>
-        <Navbar/>
+        <Navbar />
         <Routes>
-          <Route path='/' exact component={<Home/>} element={<Home/>}/>
-          <Route path='/signUp' exact component={<SignUp/>} element={<SignUp/>}/>
-          <Route path='/menu' exact component={<Menu/>} element={<Menu/>}/>
-          <Route path='/about' exact component={<About/>} element={<About/>}/>
-          <Route path='/contact' exact component={<Contact/>} element={<Contact/>}/>
+          <Route path='/' exact component={<Home />} element={<Home />} />
+          <Route path='/signUp' exact component={<SignUp />} element={<SignUp />} />
+          <Route path='/menu' exact component={<Menu />} element={<Menu />} />
+          <Route path='/about' exact component={<About />} element={<About />} />
+          <Route path='/contact' exact component={<Contact />} element={<Contact />} />
         </Routes>
-        <Footer/>
+        <Footer />
       </Router>
     </div>
   );
